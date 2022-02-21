3c7eb18de77d8e13caab767c51626b2aa7b17539
FranciscaLiliane <42254398+FranciscaLiliane@users.noreply.github.com>
2022-02-21 10:35:10 -0300

    Add files via upload

---
+++ Exercicio 15/Entites/Reservation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Exercicio_15.Entites.Exceptions;
+namespace Exercicio_15.Entites
+{
+    class Reservation
+    {
+        private int _roomNumber { get; set; }
+        private DateTime _checkIn { get; set; }
+        private DateTime _checkOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("check-out date must be after check-in date.");
+            }
+            _roomNumber = roomNumber;
+            _checkIn = checkIn;
+            _checkOut = checkOut;
+        }
+
+        public int duration()
+        {
+            TimeSpan duration = _checkOut.Subtract(_checkIn);
+            return (int)duration.TotalDays;
+        }
+        public void updateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("reservation date for update must be future dates.");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("check-out date must be after check-in date.");
+            }
+            _checkIn = checkIn;
+            _checkOut = checkOut;
+        }
+        public override string ToString()
+        {
+            return "Room "
+                + _roomNumber
+                + ", check-in: "
+                + _checkIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + _checkOut.ToString("dd/MM/yyyy")
+                + ", "
+                +duration()
+                + " nights";
+        }
+    }
+}
